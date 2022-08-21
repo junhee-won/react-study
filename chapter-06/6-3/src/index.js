f73b9bdfca9f9e5bed7ce84d6d06cc7dcb2ee5aa
junhee-won <wwoon63@snu.ac.kr>
2022-08-21 14:03:04 +0900

    redux 적용

--- chapter-06/6-3/src/index.js
+++ chapter-06/6-3/src/index.js
@@ -1,6 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createStore, combineReducers } from "redux";
+import timelineReducer, {
+  addTimeline,
+  removeTimeline,
+  editTimeline,
+  increaseNextPage,
+} from "./timeline/state";
+import friendReducer, {
+  addFriend,
+  removeFriend,
+  editFriend,
+} from "./friend/state";
+
+const reducer = combineReducers({
+  timeline: timelineReducer,
+  friend: friendReducer,
+});
+const store = createStore(reducer);
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch(addTimeline({ id: 1, desc: "코딩코딩" }));
+store.dispatch(increaseNextPage());
+store.dispatch(addFriend({ id: 1, name: "Mike" }));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
